a4eef8141c08bdaff3f8afb5fb9e97ea3a99e742
Ernest Buffington <ernest.buffington@gmail.com>
2021-04-08 23:13:06 -0400

    Language Update
    Language Update

---
+++ language/Sommaire/lang-english.php
@@ -0,0 +1,12 @@
+<?php
+/*=======================================================================
+ Nuke-Evolution Basic: Enhanced PHP-Nuke Web Portal System
+ =======================================================================*/
+
+define_once("_SOMNEWPM","New Private Messages !!");
+define_once("_SOMSELECTALINK","Select...");
+define_once("_SOMNEWCONTENT","New content !");
+define_once("_SOMRESTRICTED","Access restricted to our members");
+define_once("_SOMMAIREADMINVIEWALLMODULES","<strong>Modules visible</strong><br />but unselected");
+
+?>
