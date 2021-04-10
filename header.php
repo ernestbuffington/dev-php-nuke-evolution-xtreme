04b3ef0596159e11e18075cbe296a0fe809b27f2
Ernest Buffington <ernest.buffington@gmail.com>
2021-04-09 20:56:35 -0400

    Header Update
    Header Update

--- header.php
+++ header.php
@@ -51,7 +51,7 @@ function head()
 	else 
 	{
       echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd" />'."\n";
-      echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="'._LANGCODE.'">'."\n";
+      echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="'._LANGCODE.'" />'."\n";
       echo '<html xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="https://www.facebook.com/2008/fbml" />'."\n"; 
       echo "<!-- START <head> -->\n";
       echo '<head>'."\n";
