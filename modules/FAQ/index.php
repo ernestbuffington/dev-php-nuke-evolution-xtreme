b8331387803073b465605313426239cab317e149
Ernest Buffington <ernest.buffington@gmail.com>
2021-05-06 09:38:46 -0400

    Update index.php

--- modules/FAQ/index.php
+++ modules/FAQ/index.php
@@ -41,80 +41,111 @@ get_lang($module_name);
 
 $pagetitle = '- '.$module_name;
 
-function ShowFaq($id_cat, $categories) {
+function ShowFaq($id_cat, $categories) 
+{
     global $bgcolor2, $sitename, $prefix, $db, $module_name;
     $categories = htmlentities($categories);
     OpenTable();
+
+    # stop using <br /> use a div tag with padding whenever it's possible!
+    print '<div align="center" style="padding-top:11px;">'."\n"; 
+    print '</div>'."\n";
+	
     echo"<center><span class=\"content\"><strong>$sitename "._FAQ2."</strong></span></center><br /><br />"
         ."<a name=\"top\"></a><br />" /* Bug fix : added missing closing hyperlink tag messes up display */
         .""._CATEGORY.": <a href=\"modules.php?name=$module_name\">"._MAIN."</a> -> $categories"
         ."<br /><br />"
         ."<table width=\"100%\" cellpadding=\"4\" cellspacing=\"0\" border=\"0\">"
         ."<tr bgcolor=\"$bgcolor2\"><td colspan=\"2\"><span class=\"option\"><strong>"._QUESTION."</strong></span></td></tr><tr><td colspan=\"2\">";
-    $id_cat = intval($id_cat);
+    
+	$id_cat = intval($id_cat);
     $result = $db->sql_query("SELECT id, id_cat, question, answer FROM ".$prefix."_faqanswer WHERE id_cat='$id_cat'");
-    while ($row = $db->sql_fetchrow($result)) {
+
+    while ($row = $db->sql_fetchrow($result)):
         $id = intval($row['id']);
         $id_cat = intval($row['id_cat']);
         $question = stripslashes(check_html($row['question'], "nohtml"));
         $answer = stripslashes($row['answer']);
         echo"<strong><big>&middot;</big></strong>&nbsp;&nbsp;<a href=\"#$id\">$question</a><br />";
-    }
+    endwhile;
+
     echo "</td></tr></table>
     <br />";
 }
 
-function ShowFaqAll($id_cat) {
+function ShowFaqAll($id_cat) 
+{
     global $bgcolor2, $prefix, $db, $module_name;
     $id_cat = intval($id_cat);
+
     echo "<table width=\"100%\" cellpadding=\"4\" cellspacing=\"0\" border=\"0\">"
         ."<tr bgcolor=\"$bgcolor2\"><td colspan=\"2\"><span class=\"option\"><strong>"._ANSWER."</strong></span></td></tr>";
     $result = $db->sql_query("SELECT id, id_cat, question, answer FROM ".$prefix."_faqanswer WHERE id_cat='$id_cat'");
-    while ($row = $db->sql_fetchrow($result)) {
+
+    while ($row = $db->sql_fetchrow($result)): 
+	
         $id = intval($row['id']);
         $id_cat = intval($row['id_cat']);
          $question = stripslashes(check_html($row['question'], "nohtml"));
          $answer = stripslashes($row['answer']);
          $answer = decode_bbcode(set_smilies(stripslashes($answer)), 1, true);
-        echo"<tr><td><a name=\"$id\"></a>"
+    
+	    echo"<tr><td><a name=\"$id\"></a>"
             ."<strong><big>&middot;</big></strong>&nbsp;&nbsp;<strong>$question</strong>"
             ."<p align=\"justify\">$answer</p>"
             ."[ <a href=\"#top\">"._BACKTOTOP."</a> ]"
             ."<br /><br />"
             ."</td></tr>";
-    }
-    echo "</table><br /><br />"
+    
+	endwhile;
+    
+	echo "</table><br /><br />"
         ."<div align=\"center\"><strong>[ <a href=\"modules.php?name=$module_name\">"._BACKTOFAQINDEX."</a> ]</strong></div>";
 }
 
-if (!isset($myfaq)) {
+if (!isset($myfaq)): 
+
     global $currentlang, $multilingual;
-    if ($multilingual == 1) {
+
+    if ($multilingual == 1) 
         $querylang = "WHERE flanguage='$currentlang'";
-    } else {
+	else 
         $querylang = "";
-    }
+
     include_once(NUKE_BASE_DIR.'header.php');
+
     OpenTable();
-    echo "<center><span class=\"option\">"._FAQ2."</span></center><br /><br />"
+    
+    # stop using <br /> use a div tag with padding whenever it's possible!
+    print '<div align="center" style="padding-top:11px;">'."\n";
+    print '</div>'."\n";
+		
+	echo "<center><span class=\"option\"><h1>"._FAQ2."</h1></span></center><br /><br />"
     ."<table width=\"100%\" cellpadding=\"4\" cellspacing=\"0\" border=\"0\">"
     ."<tr><td bgcolor=\"$bgcolor2\"><span class=\"option\"><strong>"._CATEGORIES."</strong></span></td></tr><tr><td>";
     $result2 = $db->sql_query("SELECT id_cat, categories FROM ".$prefix."_faqcategories $querylang");
-    while ($row2 = $db->sql_fetchrow($result2)) {
-    $id_cat = intval($row2['id_cat']);
-    $categories = stripslashes(check_html($row2['categories'], "nohtml"));
-    $catname = urlencode($categories);
-    echo"<strong><big>&middot;</big></strong>&nbsp;<a href=\"modules.php?name=$module_name&amp;myfaq=yes&amp;id_cat=$id_cat&amp;categories=$catname\">$categories</a><br />";
-    }
+
+    while ($row2 = $db->sql_fetchrow($result2)):
+     $id_cat = intval($row2['id_cat']);
+     $categories = stripslashes(check_html($row2['categories'], "nohtml"));
+     $catname = urlencode($categories);
+     echo"<strong><big>&middot;</big></strong>&nbsp;<a href=\"modules.php?name=$module_name&amp;myfaq=yes&amp;id_cat=$id_cat&amp;categories=$catname\">$categories</a><br />";
+    endwhile;
+	
     echo "</td></tr></table>";
+	
+   # stop using <br /> use a div tag with padding whenever it's possible!
+    print '<div align="center" style="padding-top:11px;">'."\n";
+    print '</div>'."\n";
+	
     CloseTable();
+
     include_once(NUKE_BASE_DIR.'footer.php');
-} else {
+else:
     include_once(NUKE_BASE_DIR.'header.php');
     ShowFaq($id_cat, $categories);
     ShowFaqAll($id_cat);
     CloseTable();
     include_once(NUKE_BASE_DIR.'footer.php');
-}
-
+endif;
 ?>
