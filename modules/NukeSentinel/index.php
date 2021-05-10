0831cee0ce2b5331c1abad7c19c6425d280d7d73
Ernest Buffington <ernest.buffington@gmail.com>
2021-05-10 02:07:54 -0400

    Update index.php

--- modules/NukeSentinel/index.php
+++ modules/NukeSentinel/index.php
@@ -10,7 +10,7 @@
 /* See CREDITS.txt for ALL contributors                 */
 /********************************************************/
 if(!defined('MODULE_FILE')) die ('You can\'t access this file directly...');
-define('NUKESENTINEL_PUBLIC', true);
+define('NUKESENTINEL_PUBLIC',true);
 $module_name = basename(dirname(__FILE__));
 $ab_config = abget_configs();
 $checkrow = $db->sql_numrows($db->sql_query('SELECT `ip_lo` FROM `'.$prefix.'_nsnst_ip2country` LIMIT 0,1'));
@@ -18,12 +18,12 @@ if($checkrow > 0) $tableexist = 1; else $tableexist = 0;
 if(!isset($op)) $op='';
 if($op == 'STIP2C' AND $tableexist != 1) $op = 'STIndex';
 if(!$op) $op = 'STIndex';
-include_once(NUKE_MODULES_DIR . $module_name . '/public/functions.php');
+include_once(NUKE_MODULES_DIR.$module_name.'/public/functions.php');
 switch($op):
-  case 'STIndex':include(NUKE_MODULES_DIR . $module_name . '/public/STIndex.php');break;
-  case 'STIPS':include(NUKE_MODULES_DIR . $module_name . '/public/STIPS.php');break;
-  case 'STRanges':include(NUKE_MODULES_DIR . $module_name . '/public/STRanges.php');break;
-  case 'STReferers':include(NUKE_MODULES_DIR . $module_name . '/public/STReferers.php');break;
-  case 'STIP2C':include(NUKE_MODULES_DIR . $module_name . '/public/STIP2C.php');break;
+  case 'STIndex':include(NUKE_MODULES_DIR.$module_name.'/public/STIndex.php');break; 
+  case 'STIPS':include(NUKE_MODULES_DIR.$module_name.'/public/STIPS.php');break;
+  case 'STRanges':include(NUKE_MODULES_DIR.$module_name.'/public/STRanges.php');break;
+  case 'STReferers':include(NUKE_MODULES_DIR.$module_name.'/public/STReferers.php');break;
+  case 'STIP2C':include(NUKE_MODULES_DIR.$module_name.'/public/STIP2C.php');break;
 endswitch;
 ?>
