e2bcd663b80e70e3615534cc69a3535660744922
Ernest Buffington <ernest.buffington@gmail.com>
2021-05-13 17:50:43 -0400

    Update ya_tos.php

--- modules/Your_Account/public/ya_tos.php
+++ modules/Your_Account/public/ya_tos.php
@@ -1,24 +1,24 @@
 <?php
-/*======================================================================= 
-  PHP-Nuke Titanium | Nuke-Evolution Xtreme : PHP-Nuke Web Portal System
- =======================================================================*/
+/*=============================================================================== 
+   PHP-Nuke Titanium | Nuke-Evolution Xtreme : PHP-Nuke Web Portal System
+ ================================================================================*/
 
 /*********************************************************************************/
-/* CNB Your Account: An Advanced User Management System for phpnuke             */
-/* ============================================                                 */
-/*                                                                              */
-/* Copyright (c) 2004 by Comunidade PHP Nuke Brasil                             */
-/* http://dev.phpnuke.org.br & http://www.phpnuke.org.br                        */
-/*                                                                              */
-/* Contact author: escudero@phpnuke.org.br                                      */
-/* International Support Forum: http://ravenphpscripts.com/forum76.html         */
-/*                                                                              */
-/* This program is free software. You can redistribute it and/or modify         */
-/* it under the terms of the GNU General Public License as published by         */
-/* the Free Software Foundation; either version 2 of the License.               */
-/*                                                                              */
+/* CNB Your Account: An Advanced User Management System for phpnuke              */
+/* ============================================                                  */
+/*                                                                               */
+/* Copyright (c) 2004 by Comunidade PHP Nuke Brasil                              */
+/* http://dev.phpnuke.org.br & http://www.phpnuke.org.br                         */
+/*                                                                               */
+/* Contact author: escudero@phpnuke.org.br                                       */
+/* International Support Forum: http://ravenphpscripts.com/forum76.html          */
+/*                                                                               */
+/* This program is free software. You can redistribute it and/or modify          */
+/* it under the terms of the GNU General Public License as published by          */
+/* the Free Software Foundation; either version 2 of the License.                */
+/*                                                                               */
 /*********************************************************************************/
-/* CNB Your Account it the official successor of NSN Your Account by Bob Marion    */
+/* CNB Your Account is the official successor of NSN Your Account by Bob Marion  */
 /*********************************************************************************/
 
 /********************************************************/
